81ace369d4b8c421452da98026f33a107ad2fdc3
Xiao Dong <162651912+XiaoDong1213@users.noreply.github.com>
2026-08-31 15:35:46 +0800

    重构搜索模块结构

---
+++ ui/search/__init__.py
@@ -0,0 +1,7 @@
+"""搜索功能模块。"""
+
+from .book_matcher import BookMatcher
+from .search_parser import SearchParser
+from .search_state import SearchState
+
+__all__ = ["BookMatcher", "SearchParser", "SearchState"]
